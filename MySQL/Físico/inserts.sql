15a65bfb960c8d19e0ff8cb0626235cb6d899b41
Ruan souza <ruanpereira020@gmail.com>
2025-05-24 19:20:28 -0300

    Banco de dados

---
+++ MySQL/Físico/inserts.sql
@@ -0,0 +1,54 @@
+INSERT INTO filmes (genero_filme_id, fil_nome, fil_diretor, fil_duracao, fil_poster, fil_dtlancamento)
+VALUES 
+(1, 'Vingadores: Ultimato', NULL, NULL, NULL, NULL),
+(1, 'Mad Max: Estrada da Fúria', NULL, NULL, NULL, NULL),
+(1, 'John Wick', NULL, NULL, NULL, NULL),
+
+(2, 'À Procura da Felicidade', NULL, NULL, NULL, NULL),
+(2, 'O Curioso Caso de Benjamin Button', NULL, NULL, NULL, NULL),
+(2, 'Forrest Gump', NULL, NULL, NULL, NULL),
+
+(3, 'Harry Potter', NULL, NULL, NULL, NULL),
+(3, 'Senhor dos Anéis', NULL, NULL, NULL, NULL),
+(3, 'Alice no País das Maravilhas', NULL, NULL, NULL, NULL),
+
+(4, 'Superbad', NULL, NULL, NULL, NULL),
+(4, 'Se Beber, Não Case!', NULL, NULL, NULL, NULL),
+(4, 'A Mentira', NULL, NULL, NULL, NULL),
+
+(5, 'Invocação do Mal', NULL, NULL, NULL, NULL),
+(5, 'O Exorcista', NULL, NULL, NULL, NULL),
+(5, 'It: A Coisa', NULL, NULL, NULL, NULL),
+
+(6, 'Diário de uma Paixão', NULL, NULL, NULL, NULL),
+(6, 'Orgulho e Preconceito', NULL, NULL, NULL, NULL),
+(6, 'Titanic', NULL, NULL, NULL, NULL);
+
+
+INSERT INTO livros (genero_livro_id, liv_nome, liv_escritor, liv_paginas, liv_capa, liv_dtlancamento)
+VALUES 
+(1, 'O Conde de Monte Cristo', NULL, NULL, NULL, NULL),
+(1, 'Jogos Vorazes', NULL, NULL, NULL, NULL),
+(1, 'O Código Da Vinci', NULL, NULL, NULL, NULL),
+
+(2, 'O diário de Anne Frank', NULL, NULL, NULL, NULL),
+(2, 'Os Miseráveis', NULL, NULL, NULL, NULL),
+(2, 'O Sol é para Todos', NULL, NULL, NULL, NULL),
+
+(3, 'Harry Potter e a Pedra Filosofal', NULL, NULL, NULL, NULL),
+(3, 'O Hobbit', NULL, NULL, NULL, NULL),
+(3, 'As Crônicas de Nárnia', NULL, NULL, NULL, NULL),
+
+(4, 'O Guia do Mochileiro das Galáxias', NULL, NULL, NULL, NULL),
+(4, 'Dom Casmurro', NULL, NULL, NULL, NULL),
+(4, 'Triste Fim de Policarpo Quaresma', NULL, NULL, NULL, NULL),
+
+(5, 'Drácula', NULL, NULL, NULL, NULL),
+(5, 'Frankenstein', NULL, NULL, NULL, NULL),
+(5, 'O Iluminado', NULL, NULL, NULL, NULL),
+
+(6, 'Orgulho e Preconceito', NULL, NULL, NULL, NULL),
+(6, 'Eleanor & Park', NULL, NULL, NULL, NULL),
+(6, 'A Culpa é das Estrelas', NULL, NULL, NULL, NULL);
+
+
